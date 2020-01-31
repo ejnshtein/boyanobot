0416a724b634a78f6723d9a1bb3dec499c1922be
ejnshtein <ejnshtein@gmail.com>
2020-01-31 14:53:31 -0800

    fix

--- core/bot.js
+++ core/bot.js
@@ -1,4 +1,3 @@
-// require('dotenv').config({ path: '../.env' })
 import Telegraf from 'telegraf-esm'
 import collection from './database/index.js'
 import logger from './database/logger.js'
@@ -15,9 +14,33 @@ bot.context.db = collection
 
 bot.use(logger)
 
+// Thanks https://github.com/backmeupplz/voicy/blob/master/middlewares/checkDate.js
 bot.use((ctx, next) => {
-  if (ctx.message && ctx.message.date > startDate - 20000) {
-    return next()
+  if (ctx.update.callback_query) {
+    next()
+    return
+  }
+  const message =
+    ctx.update.message || ctx.update.channel_post || ctx.update.callback_query
+  if (!message) {
+    if (!ctx.update.edited_message && !ctx.update.edited_channel_post) {
+      console.info(
+        'Not processing because no message found',
+        JSON.stringify(ctx.update, undefined, 2)
+      )
+    }
+    return
+  }
+  const isMsgNew = Date.now() / 1000 - message.date < 5 * 60
+  if (!isMsgNew) {
+    console.info(
+      'Not processing message',
+      message.date,
+      JSON.stringify(message, undefined, 2)
+    )
+  }
+  if (isMsgNew) {
+    next()
   }
 })
 
