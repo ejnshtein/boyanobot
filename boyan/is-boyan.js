3d121d1fd1e2da4f3aa6b8d98005e57ea85bd9ce
ejnshtein <ejnshtein@gmail.com>
2020-02-13 08:23:25 -0800

    performance hooks

--- boyan/is-boyan.js
+++ boyan/is-boyan.js
@@ -2,6 +2,8 @@ import collection from '../core/database/index.js'
 import imghash from 'imghash'
 import leven from 'leven'
 import request from '@ejnshtein/smol-request'
+import { performance } from 'perf_hooks'
+import argv from '../lib/argv.js'
 
 export const isBoyan = async ({
   chat: { id: chatId },
@@ -13,7 +15,23 @@ export const isBoyan = async ({
   const hash = await imghash.hash(buffer, 32)
   const boyans = await collection('boyans').find({ chat_id: chatId, picture_hash: { $exists: true } }, 'picture_hash message_id')
 
+  const startTime = performance.now()
+  // let boyan = null
+  // for (let i = 0; i < boyans.length; i++) {
+  //   const b = boyans[i]
+  //   const { picture_hash } = b
+  //   const diff = leven(picture_hash, hash)
+  //   // if (diff < 20) {
+  //   //   console.log(diff)
+  //   // }
+  //   if (diff <= 10 && !boyan) {
+  //     boyan = b
+  //     break
+  //   }
+  // }
+
   const boyan = boyans.find(({ picture_hash }) => {
+
     const diff = leven(picture_hash, hash)
     // if (diff < 20) {
     //   console.log(diff)
@@ -21,6 +39,10 @@ export const isBoyan = async ({
     return diff <= 10
   })
 
+  const endTime = performance.now()
+  if (argv('--debug')) {
+    console.log(`Processed ${boyans.length} boyans in ${endTime - startTime} ms.`)
+  }
   if (boyan) {
     // console.log(boyan, chatId, messageId, from)
     await collection('boyans').create({
