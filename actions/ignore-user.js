8be3b1f8f780bea9de783eb854f546999cba2ca4
ejnshtein <ejnshtein@gmail.com>
2020-02-29 15:31:28 +0200

    unignoreuser

--- actions/ignore-user.js
+++ actions/ignore-user.js
@@ -43,5 +43,44 @@ composer.command(
     )
   )
 )
+composer.command(
+  'unignoreuser',
+  Composer.groupChat(
+    Composer.branch(
+      async ctx => {
+        const { status } = await ctx.getChatMember(ctx.from.id)
+        return ['administrator', 'creator'].includes(status) || ctx.from.id === env.ADMIN_ID
+      },
+      async ctx => {
+        if (
+          !(ctx.message.reply_to_message && ctx.message.reply_to_message.message_id)
+        ) {
+          return ctx.reply('You must reply to user message.')
+        }
+        const fromId = ctx.message.reply_to_message.from.id
+        const { ok } = await ctx.db('chats').updateOne(
+          { id: ctx.chat.id },
+          { $pull: { ignored_users: fromId } }
+        )
+        if (ok) {
+          return ctx.reply('Done.', {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: 'ok',
+                    callback_data: 'delete'
+                  }
+                ]
+              ]
+            }
+          })
+        }
+        return ctx.reply('Something went wrong...')
+      },
+      ctx => ctx.reply('You are not an admin.')
+    )
+  )
+)
 
 bot.use(composer.middleware())
